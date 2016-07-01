7989d83813dd3baad4abff10ac4e4d54d4000531
cubiclesoft <webmaster@cubiclesoft.com>
2016-07-01 07:26:19 -0700

    Updated.

---
+++ readme_src/README.md
@@ -0,0 +1,20 @@
+CubicleSoft Namespaced PHP Libraries
+====================================
+
+A single repository containing all CubicleSoft PHP libraries inside a CubicleSoft namespace.  Fully automated nightly updates.  MIT or LGPL, your choice.
+
+Classes
+-------
+
+Included and supported:
+
+@INCLUDECLASSES@
+
+Other:
+
+@OTHERCLASSES@
+
+Sources
+-------
+
+@SOURCES@
---
+++ update.php
@@ -0,0 +1,88 @@
+<?php
+	// This runs the main updater.  Requires PHP and git on the path and repo commit access.  That last part you, of course, don't have.
+	// (C) 2016 CubicleSoft.  All Rights Reserved.
+
+	if (!isset($_SERVER["argc"]) || !$_SERVER["argc"])
+	{
+		echo "This file is intended to be run from the command-line.";
+
+		exit();
+	}
+
+	$rootpath = str_replace("\\", "/", dirname(__FILE__));
+
+	$srcpath = $rootpath . "/repos";
+	$destpath = $rootpath . "/support";
+
+	if (!is_dir($srcpath))  mkdir($srcpath);
+	if (!is_dir($destpath))  mkdir($destpath);
+
+	require_once $rootpath . "/repos/php-libs/functions.php";
+
+	// Update the registered repo list.  If nothing has changed, exit.
+	$numchanged = (int)GitRepoChanged($rootpath) + GitPull($srcpath);
+	if (!$numchanged)  exit();
+
+	// Always do a full rebuild.
+	DeleteDirectory($destpath);
+
+	// Retrieve a list of all PHP files that contain 'class' + a name.
+	$files = array();
+	GetPHPFiles($files, $srcpath);
+
+	// Generate final file set.
+	$globalclasses = array(
+		"stdClass",
+		"PDO",
+		"Exception",
+		"Net_DNS2_Resolver",
+	);
+
+	foreach ($files as $name => $filename)
+	{
+		$data = "<" . "?php\n";
+		$data .= "\tnamespace CubicleSoft;\n";
+		$data .= "?" . ">";
+		$data .= file_get_contents($filename);
+
+		// Replace class references with CubicleSoft + class references.
+		foreach ($files as $name2 => $filename2)
+		{
+			$data = str_replace("\"" . $name2 . "\"", "\"\\\\CubicleSoft\\\\" . $name2 . "\"", $data);
+			$data = str_replace(" extends " . $name2, " extends \\CubicleSoft\\" . $name2, $data);
+			$data = str_replace("\"" . $name2 . "::", "\"\\\\CubicleSoft\\\\" . $name2 . "::", $data);
+			$data = str_replace("'" . $name2 . "::", "'\\CubicleSoft\\" . $name2 . "::", $data);
+			$data = str_replace($name2 . "::", "\\CubicleSoft\\" . $name2 . "::", $data);
+			$data = str_replace(" new " . $name2 . "(", " new \\CubicleSoft\\" . $name2 . "(", $data);
+		}
+
+		// Handle global namespace classes.
+		foreach ($globalclasses as $name2)
+		{
+			$data = str_replace("\"" . $name2 . "\"", "\"\\\\" . $name2 . "\"", $data);
+			$data = str_replace(" extends " . $name2, " extends \\" . $name2, $data);
+			$data = str_replace("\"" . $name2 . "::", "\"\\\\" . $name2 . "::", $data);
+			$data = str_replace("'" . $name2 . "::", "'\\" . $name2 . "::", $data);
+			$data = str_replace($name2 . "::", "\\" . $name2 . "::", $data);
+			$data = str_replace(" new " . $name2 . "(", " new \\" . $name2 . "(", $data);
+		}
+
+		$pos = strrpos($filename, "/");
+		$name = substr($filename, $pos + 1);
+
+		file_put_contents($destpath . "/" . $name, $data);
+	}
+
+	// Net_DNS2.
+	mkdir($destpath . "/Net");
+	copy($srcpath . "/php-libs/support/Net/DNS2.php", $destpath . "/Net/DNS2.php");
+	copy($srcpath . "/php-libs/support/Net/license.txt", $destpath . "/Net/license.txt");
+
+	// CA certificates.
+	copy($srcpath . "/php-libs/support/cacert.pem", $destpath . "/cacert.pem");
+
+	// Generate README.
+	GenerateReadme($srcpath . "/php-libs/readme_src/classes.json", $rootpath . "/readme_src/README.md", $rootpath . "/README.md", "\\\\CubicleSoft\\\\", $files, "support/", $srcpath);
+
+	CommitRepo($rootpath);
+?>
